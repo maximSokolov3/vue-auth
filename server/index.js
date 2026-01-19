55615f85fab104b096a31cfbf72e3fe62f2769a9
Celian <nik.sir2018@yandex.ru>
2026-01-19 21:23:13 +0300

    завернул проект в докер

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(cors({
     credentials: true,
     origin: process.env.CLIENT_URL
 }));
-app.use('/api', router);
+app.use('/', router);
 app.use(errorMiddleware);
 
 const start = async () => {
